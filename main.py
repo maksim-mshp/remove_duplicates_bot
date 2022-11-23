beda4da9a5a44ab4aaa18f58e3df22d3718d3e6a
Максим Чернышков <maksim-mshp@yandex.ru>
2022-11-23 18:15:46 +0300

    main logic

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import json
 import telebot
-import json
+from storage import Storage
 
 with open("config.json") as f:
   token = json.loads(f.read())["token"]
 
 bot = telebot.TeleBot(token)
+s = Storage()
 
 
 @bot.message_handler()
 def echo_all(message):
-  bot.delete_message(message.chat.id, message.id)
+  if s.is_exists(message.chat.id, message.text):
+    bot.delete_message(message.chat.id, message.id)
+  else:
+    s.add(message.chat.id, message.text)
 
 bot.polling()
