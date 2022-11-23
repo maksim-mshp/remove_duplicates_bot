5d9730ae22def1d9b1c8e2d6139f9d3ea7ec10f9
Максим Чернышков <maksim-mshp@yandex.ru>
2022-11-23 18:10:44 +0300

    new methods for storage

--- storage.py
+++ storage.py
@@ -1,18 +1,33 @@
+from calendar import c
 import json
 import os
 
 class Storage:
   def __init__(self) -> None:
     self.filename = "data.json"
-    self.data = []
+    self.data = {}
     
     if os.path.exists(self.filename):
       with open(self.filename) as f:
         self.data = json.loads(f.read())
   
+  def save(self) -> None:
+    with open(self.filename, "w") as f:
+      f.write(json.dumps(self.data))
+  
   def get_all(self) -> list:
     return self.data
   
-if __name__ == "__main__":
-  s = Storage()
-  print(s.get_all())
+  def is_exists(self, chat_id, text) -> bool:
+    chat_id = str(chat_id)
+    if chat_id in self.data:
+      return text in self.data[chat_id]
+    return False
+  
+  def add(self, chat_id, text) -> None:
+    chat_id = str(chat_id)
+    if chat_id in self.data:
+      self.data[chat_id].append(text)
+    else:
+      self.data.update([(chat_id, [text])])
+    self.save()
